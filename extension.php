5519a18febd0913ad31bbacfb9eb7369be8d56a3
SHU-red <schuhmann.sebastian@gmail.com>
2026-04-04 17:40:12 +0200

    fix: use string hook name for compatibility

--- extension.php
+++ extension.php
@@ -8,11 +8,8 @@ class EnhancedNavigationExtension extends Minz_Extension {
     public function init(): void {
         $this->registerTranslates();
 
-        if (version_compare(FRESHRSS_VERSION, '1.28') >= 0) {
-            $this->registerHook(Minz_HookType::NavEntries, [$this, 'generateEnhancedNavigation']);
-        } else {
-            $this->registerHook('nav_entries', [$this, 'generateEnhancedNavigation']);
-        }
+        // Use string hook name for compatibility with all versions
+        $this->registerHook('nav_entries', [$this, 'generateEnhancedNavigation']);
 
         Minz_View::appendStyle($this->getFileUrl('navigation.css', 'css'));
         Minz_View::appendScript($this->getFileUrl('navigation.js', 'js'));
